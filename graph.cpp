f63718e0ba64c679fbda824772e7069e76a2f469
docsven <IMchuanqi@foxmail.com>
2022-07-04 19:59:59 +0800

    graph类

--- graph.cpp
+++ graph.cpp
@@ -1,6 +1,37 @@
 #include "graph.h"
+#include<QPainter>
+#include"qpainter.h"
 
-graph::graph()
-{
+
+//数据
+void   data(){
+
+
+
+
+/*
+    zgraph a1(4),a2(3),a3(4),a5(4),a6(4),a7(4),a8(3);
+
+
+    QPoint aop11(36,85), aop12(162,84), aop13(179,184), aop14(39,129);
+    QPoint aop21(224,83),aop22(247,178),aop23(221,83);
+
+    a1.pointdata[1]=aop11;
+    a1.pointdata[2]=aop12;*/
+    //graph m1(8);
+
+   // m1.xgrapgh[1]=a1;
+
 
 }
+
+
+
+
+
+
+
+
+zgraph::zgraph(){};
+
+graph::graph(){};
--- graph.h
+++ graph.h
@@ -1,11 +1,34 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+#include <QVector>
+#include<QPoint>
 
 
-class graph
+class zgraph
+{
+public:
+    zgraph();
+    zgraph(int n){pointnum=n;};
+    zgraph(const zgraph& exmple){pointnum=exmple.pointnum;pointdata.append(exmple.pointdata);};
+
+    int graphnum;//障碍物总数
+    QVector<QPoint> pointdata;//障碍物图形中的点数据
+    int pointnum;//障碍物图形中的点的数量
+    //void data();
+};
+
+
+
+
+class graph:public zgraph
 {
 public:
     graph();
+    graph(int n){graphnum=n;};
+    QVector<zgraph> xgrapgh;//数组存储一幅图中障碍物图形
+    int graphnum;//障碍物图形的数量
 };
+
+
 
 #endif // GRAPH_H
